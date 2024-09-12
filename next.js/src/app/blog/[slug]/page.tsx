0205f942494a84ded8c6aa09782da271acd6225c
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-12 15:10:12 +0630

    dynamic route

---
+++ next.js/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,6 @@
+//write function and accept params slug
+export default  function Blog({params}:{params:{slug:string}}) {
+    return (
+        <div> <h1>My Page</h1></div>
+    )
+}
--- next.js/src/components/PostFeed.tsx
+++ next.js/src/components/PostFeed.tsx
@@ -1,30 +1,8 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-// Mock component to simulate loading with setTimeout
-function DelayedPostFeed() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    // Simulate loading delay
-    const timer = setTimeout(() => {
-      setIsLoaded(true);
-    }, 2000); // Delay of 2 seconds
-
-    return () => clearTimeout(timer); // Cleanup timer on unmount
-  }, []);
-
-  if (!isLoaded) {
-    // Component is still loading
-    throw new Promise(() => {});
-  }
-
+"use client"
+export default function PostFeed() {
   return (
     <div>
-      Hello PostFeed
+        Hello PostFeed
     </div>
-  );
+  )
 }
-
-export default DelayedPostFeed;
