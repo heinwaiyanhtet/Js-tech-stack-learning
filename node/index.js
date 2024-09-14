de64ee03d5a833af50dc63805aada41e9e7f678e
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-09-14 13:06:59 +0630

    stdin test

--- node/index.js
+++ node/index.js
@@ -1,3 +1,4 @@
+const { Console } = require('node:console');
 const http = require('node:http');
 const fs = require('node:fs').promises;
 
@@ -12,38 +13,95 @@ const server = http.createServer((req, res) => {
   res.end('Hello, World!\n');
 });
 
-async function readFile () {
-    try {
-
-        const data = await fs.readFile('file.txt','UTF-8');
-        console.log('File content:', data);
-
-
-    } catch (error) {
-        
-    }
-}
-
-readFile();
-
-fs.readFile('./file.txt','utf-8',(err,data) => {
-     if(err){
-        console.log('Error reading the file:',err);
-        return;
-     }
-     console.log('File content:', data);
-})
-
-console.log("hello");
 
 
 
-try {
-    const data = fs.readFileSync('./file.txt', 'utf-8');
-    console.log('File content:', data);
-} catch (error) {
+
+// global.myGlobalVar = 'Hello,Global!';
+
+// console.log(global.myGlobalVar);
+
+
+
+// process.stdin.setEncoding('utf-8');
+
+
+// console.log("Please Enter Some text");
+
+
+
+// process.stdin.on('data', (data) => {
+
+
+//     const input = data.trim();
     
-}
+//     if(input === 'exit')
+//     {
+//         console.log('Exiting the program...');
+//         process.exit();
+//     }
+//     else
+//     {
+//       console.log(`you entered: ${input}`);
+//     }
+
+
+// });
+
+
+
+
+
+
+// event handling 
+// const eventEmitter = require('node:events');
+
+// const myEmitter = new eventEmitter();
+
+// myEmitter.on('customEvent',(arg) => 
+// {
+//     console.log('Event occurred with argument:',arg);
+// })
+
+// myEmitter.emit('customEvent', 'Sample Argument');
+
+
+
+
+
+// async function readFile () {
+//     try {
+
+//         const data = await fs.readFile('file.txt','UTF-8');
+//         console.log('File content:', data);
+
+
+//     } catch (error) {
+        
+//     }
+// }
+
+// readFile();
+
+// fs.readFile('./file.txt','utf-8',(err,data) => {
+//      if(err){
+//         console.log('Error reading the file:',err);
+//         return;
+//      }
+//      console.log('File content:', data);
+// })
+
+// console.log("hello");
+
+
+
+// try {
+//     const data = fs.readFileSync('./file.txt', 'utf-8');
+//     console.log('File content:', data);
+// } catch (error) {
+    
+// }
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
